66e926f64659b45ad2819e125bce0abb12bb6f42
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-11-20 14:44:37 -0600

    10. Variables exercise 01 concluded

---
+++ JavaScript_Basics/variables_exercise_01.js
@@ -0,0 +1,7 @@
+// In index.js, please define TWO variables:
+
+// - Define a variable called "myLuckyNumber" and set it equal ti 37.
+// - Define a variable called "octopusLimbs" and set it equal ti 8.
+
+let myLuckyNumber = 37;
+let octopusLimbs = 8;

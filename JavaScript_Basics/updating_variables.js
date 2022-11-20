e928d915cce765a48b5b947d29bdff29d691df05
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-11-20 14:53:10 -0600

    11. Updating variables concluded

---
+++ JavaScript_Basics/updating_variables.js
@@ -0,0 +1,27 @@
+let score = 0;
+
+// Here's a more easy and short way to update the value
+// of a variable:
+
+score += 5;
+
+// This apporach can also be used with other operators:
+
+score -= 50;
+score *= 20;
+score /= 2;
+
+// Examples
+
+// Number of lives in a game:
+let numLives = 9;
+
+// You lose a live:
+numLives -= 1;
+
+// A more short and easy way to decreament a live in this
+// example game, could be:
+numLives--;
+
+// It also works the other way around, you won an extra life:
+numLives++;

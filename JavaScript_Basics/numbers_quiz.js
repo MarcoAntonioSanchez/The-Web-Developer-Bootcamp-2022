f72493e47ab6a981f1e72f9ce09de3416f5c034d
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-11-20 14:25:43 -0600

    07. Numbers quick quiz concluded

---
+++ JavaScript_Basics/numbers_quiz.js
@@ -0,0 +1,20 @@
+// What does this evaluate to?
+
+4 +
+  (3 * 4) /
+    2(
+      // Answer: 14
+
+      // Correct answer is 10, beacuse in order of operations
+      // the multiplication and division take place first
+      // before the addittion.
+
+      13 % 5
+    ) **
+      2;
+
+// Answer: 9
+
+200 + 0 / 0;
+
+// Answer: NaN

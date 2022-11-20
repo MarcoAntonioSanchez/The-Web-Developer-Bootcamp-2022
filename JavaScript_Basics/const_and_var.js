fb0f328cc2a6a604d072232fb7b1d386ede09845
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-11-20 15:13:16 -0600

    12. Const and var concluded

---
+++ JavaScript_Basics/const_and_var.js
@@ -0,0 +1,33 @@
+// Const
+
+// Const works just like let, except you CANNOT change
+// the value
+
+// NOT ALLOWED!
+
+const hens = 4;
+hens = 20; //ERROR!
+
+const age = 17;
+age = age + 1; //ERROR!
+
+// Why use const?
+
+const pi = 3.14159;
+const daysInWeek = 7;
+const minHeightForRide = 60;
+
+// Once we cover Arrays & Objects, we'll see other
+// situations where const makes sens over let.
+
+const feetInMile = 5280;
+
+// Var
+
+// The old variables keyword
+// Before let & const, var was the only way of declaring
+// variables. These days there isn't really a reason to use it.
+
+var runDIstance = 26.2;
+
+// There are really no good reasons to use var before let or const.

dd33d43f09964f66a87f64928bacec9dfe0b81db
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-11-20 14:19:19 -0600

    06. Not a number concluded

---
+++ JavaScript_Basics/nan.js
@@ -0,0 +1,14 @@
+// Not a Number
+
+// NaN is a numeric value that represents something that
+// is not a number
+
+// Examples
+
+0 / 0; //NaN
+1 + NaN; //NaN
+
+// Using the operator: typeof you can get the type
+// of value writed next to the operator:
+
+typeof 2; //"number"

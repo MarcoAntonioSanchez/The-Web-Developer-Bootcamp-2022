18893edece26537fef317861262c5c79289a534c
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-12-22 15:12:18 -0600

    15. Constants exercise concluded

---
+++ JavaScript_Basics/constants_exercise_02.js
@@ -0,0 +1,10 @@
+// First constants exercise:
+
+// Let's get some practice defining constants.
+// Please define the following two variables:
+
+// 1. Define a constant called "boilingPointC" and set it to 100.
+const boilingPointC = 100;
+
+// 2. Define a second constant called "boilingPointF" and set it to 212.
+const boilingPointF = 212;

72696686a5676b2d3305cca4f5adce2ae4afc935
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-12-22 16:40:44 -0600

    21. Strings basic quiz concluded

---
+++ JavaScript_Basics/strings_basic_quiz_exercise_06.js
@@ -0,0 +1,28 @@
+// Strings basic quiz:
+
+// Question 1:
+// Given the following string:
+
+const animal = "hippopotamus";
+
+// Which character am I accessing?
+animal[7];
+
+// Answer: "t".
+
+// Question 2:
+// What is stored in the combo variable?
+
+const city = "Kyoto";
+const country = "Japan";
+const combo = city + country;
+
+// Answer: "KyotoJapan"
+
+// Question 3:
+// What does the following return?
+
+let year = "1998";
+year + 1;
+
+// Answer: "19981"

15c12dc2bca29c10772279fdbc63898df2c0cb91
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-12-22 14:56:19 -0600

    13. Understanding booleans concluded

---
+++ JavaScript_Basics/booleans.js
@@ -0,0 +1,21 @@
+// Booleans
+
+// True or false
+// Booleans are very simple.
+// You have two possible options: true or false. That's it.
+
+let isLoggedIn = true;
+let gameOver = false;
+const isWaterWet = true;
+
+// true and false are uppercase sensitive.
+// In JS we must use all lowercase, for true and false values.
+
+// Variables can change types:
+
+let numPuppies = 23; // Number
+numPuppies = false; // Now a boolean
+numPuppies = 100; // Back to number
+
+// It doesn't really make sense to change from a number to a
+// boolean here, but we can!

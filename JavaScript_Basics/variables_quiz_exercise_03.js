dab101f083eaaff19cdc6ea9c71e0502dbb398ab
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-12-22 15:18:19 -0600

    16. Variables quiz concluded

---
+++ JavaScript_Basics/variables_quiz_exercise_03.js
@@ -0,0 +1,34 @@
+// Variables quiz:
+
+// Question 1:
+// What is the value of score after the following code runs?
+
+let score = 5;
+score + 4;
+
+// Answer: 5.
+// Because: The variable "score" is not updated so it won't add
+// the 4 number to the initial 5.
+
+// Question 2:
+// What is the value of bankBalance after the following code runs:
+
+let bankBalance = 100;
+bankBalance /= 2;
+bankBalance += 10;
+
+// Answer: 60.
+// Because: First we initialize the variable "bankBalance" on 100.
+// Then we devide it by 2, this give us 50 as result.
+// For last we do the addition and update on the variable with
+// a 10 over the 50 that we have first, total: 60.
+
+// Question 3:
+// What is the value of age after the following code runs?
+
+const age = 64;
+age++;
+
+// Answer: 64.
+// Because: It is a constant called "age" so we can't update
+// the value so it remains in 64.

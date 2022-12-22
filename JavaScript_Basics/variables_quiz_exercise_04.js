625f1c732be0473c333b8ba9387bc7ec73571acc
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-12-22 15:23:39 -0600

    17. Variables quiz concluded

---
+++ JavaScript_Basics/variables_quiz_exercise_04.js
@@ -0,0 +1,24 @@
+// Variables quiz:
+
+// Question 1:
+// Which one of the following variables contains a valid boolean:
+
+// let isHappy = 'true';
+// let isHappy = True;
+let isHappy = true;
+
+// Answer: the 3rd one.
+// Because: The 2st one is a string and the second one has the
+// uppercase first letter so the correct one is "true".
+
+// Question 2:
+// Which one of the following variable declarations follows JavaScript variable naming conventions?
+
+// let high_score = 107;
+// let high-score = 107;
+// let HighScore = 107;
+let highScore = 107;
+
+// Answer: the last one.
+// Because: None of the first three are cammel notation as the
+// good practice's recommends.

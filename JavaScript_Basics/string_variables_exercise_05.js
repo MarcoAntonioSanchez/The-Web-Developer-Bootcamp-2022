dba3d9d4156d34c24df61596d31588d629ed6daa
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-12-22 15:47:27 -0600

    19. String variables exercise 05 concluded

---
+++ JavaScript_Basics/string_variables_exercise_05.js
@@ -0,0 +1,10 @@
+//Our First String Variables Practice
+// Please define the following string variables (you can use either let or const):
+
+// 1. The variable bestColor should be set to the string purple (the best color on planet earth, as we all know);
+
+let bestColor = "purple";
+
+// 2. Variable quote should be set to the string You had me at "hello" - be careful about quotation marks, we want double quotes inside the string.
+
+let quote = 'You had me at "hello"';

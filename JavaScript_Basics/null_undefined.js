642a4d6cf49d77e085db07e96125eec54de89341
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-12-22 20:44:17 -0600

    27. Null and undefined concluded

---
+++ JavaScript_Basics/null_undefined.js
@@ -0,0 +1,28 @@
+// Null and Undefined
+
+// Null: International absenses of any value
+// Must be assigned
+
+// Undefined:
+// Variables that di not have an assigned value are undefined
+
+// You can get to undefined of you look for the index of an
+// unexisting character, for example:
+
+"hello"[99];
+// undefined
+
+// Another way to make it to undefined, will be declaring
+// a variable but not giving it a value, for example:
+
+let tempVariable;
+tempVariable;
+// undefined
+
+// Null
+
+// No one is logged in yet...
+let loggedInUser = null; // value is explicitly nothing
+
+// A user logs in...
+loggedInUser = "Alan Rickman";

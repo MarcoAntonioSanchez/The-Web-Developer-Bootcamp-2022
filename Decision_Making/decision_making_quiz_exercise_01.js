f76919de9d2578a24970c4156d204772582b6b37
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-12-23 16:47:09 -0600

    33. Decision making quiz exercise 01 concluded

---
+++ Decision_Making/decision_making_quiz_exercise_01.js
@@ -0,0 +1,14 @@
+// Question 1:
+// What does the following code evaluate to?
+
+3 <= 2; // false
+
+// Question 2:
+// What does the following evaluate to?
+
+9 == "9"; // true
+
+//Question 3:
+// What does the following code evaluate to?
+
+-5 >= -6; // false

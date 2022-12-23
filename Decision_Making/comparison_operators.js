12572f0636b374e1fb6550f87ebca70e11569f72
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-12-23 06:22:11 -0600

    31. Comparison operators concluded

---
+++ Decision_Making/comparison_operators.js
@@ -0,0 +1,10 @@
+// Comparison
+
+// > // greater than
+// < // less than
+// >= // greater than or equal to
+// <= // less than or qeual to
+// == // equality
+// != // not equal
+// === // strict equality
+// !== // strict non-equality

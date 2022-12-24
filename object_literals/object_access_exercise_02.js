303bb8e24f48b8ba003769ee255885d95c6fcd8f
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-12-24 15:16:09 -0600

    64. Object acces exercise 02 concluded

---
+++ object_literals/object_access_exercise_02.js
@@ -0,0 +1,34 @@
+// Object Access Exercise
+// In index.js, you'll find a restaurant object that contains
+// a name, address, city, state, and zipcode.
+
+// Your task to is to create a variable named fullAddress that
+// points to a string using the information from restaurant.
+
+// fullAddress should point to a string that includes the
+// address, city, state, and zipcode from the restaurant object.
+
+// To make it harder to "cheat" by copy and pasting,
+// I've randomly generated the address portion.
+
+// For example, if address was "64 Johnson Ave", the fullAddress
+// variable would look like this:
+// fullAddress //"64 Johnson Ave, Brooklyn, NY 11206"
+
+//PLEASE DON'T TOUCH THIS LINE!
+const restaurant = {
+  name: "Ichiran Ramen",
+  address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+  city: "Brooklyn",
+  state: "NY",
+  zipcode: "11206",
+};
+
+// With stringify objects are covnerted to the corresponding
+// primitive values during stringification
+JSON.stringify(new Date(1906, 0, 2, 15, 4, 5));
+// '"1906-01-02T15:04:05.000Z"'
+
+// Answer:
+let fullAddress = JSON.stringify(restaurant);
+// "Ichiran Ramen", "21007 Johnson Ave", "Brooklin", "NY", "11206"

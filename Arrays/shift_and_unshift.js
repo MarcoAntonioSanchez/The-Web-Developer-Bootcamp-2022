d4e9edbf388948bebfd8b65e23fd3f1c49d3eb86
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-12-24 06:21:00 -0600

    51. Shift and unshift array methods concluded

--- Arrays/shift_and_unshift.js
+++ Arrays/shift_and_unshift.js
@@ -1,1 +1,10 @@
 // Shift and unshift
+
+// Remove the first person in line
+movieLine = ["tom", "nancy", "pablo", "oliver", "eva"];
+movieLine.shift();
+movieLine; // "nancy", "pablo", "oliver", "eva"
+
+// Add a new person to the beginning of the line
+movieLine.unshift("VIP");
+movieLine; // "VIP", "tom", "nancy", "pablo", "oliver", "eva"

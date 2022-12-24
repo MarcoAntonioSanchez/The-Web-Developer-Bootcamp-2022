fa830f97498973cf3a1dc76fc548856eae546697
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-12-24 05:48:23 -0600

    47. Creating arrays concluded

---
+++ Arrays/creating_arrays.js
@@ -0,0 +1,19 @@
+//Creating arrays
+// To make an empty array
+let students = [];
+
+// An array of strings
+let colors = ["red", "blue", "black"];
+
+// An array of numbers
+let lottoNums = [12, 17, 77, 88];
+
+// A mixed array
+let struff = [true, 68, "cat", null];
+
+// Unlike other programming lenguajes, JS let us use any type
+// of values inside arrays, no matter what.
+
+let days = ["Monday", "Tuesday", "Wednesday"];
+days.length;
+// 3, referencing the 3 "things" inside the array
---
+++ Arrays/js_arrays.md
@@ -0,0 +1,5 @@
+// Arrays
+// Ordered collections of values:
+// - List of comments on IG post.
+// - Collection of levels in a game.
+// - Songs in a playlist.

8d442039ab8540b185e9befa777f1a0551dfcaa3
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-12-24 08:04:53 -0600

    61. Creating object literals concluded

---
+++ object_literals/creating_object_literals.js
@@ -0,0 +1,24 @@
+// Creating object literals
+
+typeof [];
+// "object"
+
+// Example of a common object
+const person = { firstName: "Raven", lastName: "Milosh" };
+
+// All types welcome
+const comment = {
+  username: "sillyGoose420",
+  downVotes: 19,
+  upVotes: 214,
+  netScore: 195,
+  commentText: "Tastes like chicken lol",
+  tags: ["#hilarious", "#funny", "#silly"],
+  isGilded: false,
+};
+
+const kitchenSink = {
+  favNum: 23343,
+  isFunny: true,
+  colors: ["red", "blue"],
+};

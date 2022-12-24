0fdd0d5405167b2488005eecb6141d7de7ec8b6c
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-12-24 07:14:12 -0600

    55. Sort array method

---
+++ Arrays/sort.js
@@ -0,0 +1,8 @@
+// Sort array method
+
+// You can use it to sort numbers or strings.
+// It's not a reliable method to sort numbers, for now and without
+// getting into functions, this is the only example given so far:
+let scores = [1, 70, 100, 2500, 9, -14, 0, 34];
+scores.sort();
+// -14, 0, 1, 9, 34, 70, 100, 2500

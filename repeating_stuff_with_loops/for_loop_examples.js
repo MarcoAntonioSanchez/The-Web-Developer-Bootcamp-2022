f1cc65af2e7f64e0d8ae4ddcaf67ac9553998e46
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-12-24 16:16:13 -0600

    69. For loop examples concluded

---
+++ repeating_stuff_with_loops/for_loop_examples.js
@@ -0,0 +1,25 @@
+// For loop examples
+
+// 1st example
+// Printing out even numbers from 1 to 20
+for (let i = 0; i <= 20; i += 2) {
+  console.log(i);
+}
+
+// 2nd example
+// Printing out odd numbers
+for (let i = 1; i <= 20; i += 2) {
+  console.log(i);
+}
+
+// 3rd example
+// Counting down from 100 to 0
+for (let i = 100; i >= 0; i -= 10) {
+  console.log(i);
+}
+
+// 4th example
+// Incrementing 10 by 10 to 100
+for (let i = 10; i <= 100; i *= 10) {
+  console.log(i);
+}

63787c12eff53c5176c3b18b4e980b19df4da513
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-12-24 06:16:36 -0600

    50. Push and pop array methods concluded

---
+++ Arrays/push_and_pop.js
@@ -0,0 +1,29 @@
+// Array methods
+
+// Push - add to end
+// Pop - remove from end
+// Shift - remove from start
+// Unshift - add to start
+
+// You'll get used to the names eventually
+
+// More mthods
+
+// contact - merge arrays
+// includes - look for a value
+// indexOf - kust like string.indexOf
+// join - creates a string from an array
+// reverse - reverses an array
+// slice - copies a portion on an array
+// splice - removes/replaces elements
+// sort - sorts an array
+
+let movieLine = ["Tom", "Nancy"];
+
+// Push items to the end of the movie line
+movieLine.push("Olvier");
+movieLine; // "Tom", "Nancy", "Oliver"
+
+// Remove the last person in the movie line
+movieLine.pop();
+movieLine; // "Tom", "Nancy"

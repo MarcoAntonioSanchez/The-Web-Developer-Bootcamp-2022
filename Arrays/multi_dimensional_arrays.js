8cf4d6cf1f077ddfaee562aa5de5dd1bfd382463
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-12-24 07:43:19 -0600

    58. Multi-dimensional arrays concluded

---
+++ Arrays/multi_dimensional_arrays.js
@@ -0,0 +1,22 @@
+// Multi-dimensional arrays
+
+// Wen can store arrays inside other arrays
+// two-level array:
+const colors = [
+  ["red", "crimson"],
+  ["orange, yellow"],
+  ["white", "black"],
+  ["blue", "green"],
+  ["turquise", "gray"],
+];
+
+// Nested arrays, tik tak toe game
+const board = [
+  ["O", null, "X"],
+  [null, "X", "O"],
+  ["X", "O", null],
+];
+
+// Access data a two-level nested array
+board[1][1];
+// 'X'

9bac061a409eb6aa66ef1238da47781925c9f5e6
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-12-24 22:11:53 -0600

    82. Intro tu finctions concluded

---
+++ introducing functions/intro_to_functions.js
@@ -0,0 +1,13 @@
+// Functions
+// Reusable procedures
+
+// - Functions allow us to write reusable, modular code
+// - We define a "chunk" of code that we can then execute at a later point
+// - We use them all the time
+
+// The goal will be to write down something like this:
+let die2 = rollDie();
+
+// To do something like this, every time without having to
+// re-writed
+let die1 = Math.floor(Math.random() * 6) + 1;

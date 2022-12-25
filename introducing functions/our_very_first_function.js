ece095dd4e9a8320381a71599c380ddb62362873
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-12-24 22:19:20 -0600

    83. Our very first function concluded

---
+++ introducing functions/our_very_first_function.js
@@ -0,0 +1,31 @@
+// Our very first function
+
+// Two step process:
+// 1. Define
+function funcName() {
+  // do something
+}
+
+// Example of a function
+function grumpus() {
+  console.log("ugh... you again");
+  console.log("for the last time");
+  console.log("LEAVE ME ALONE!");
+}
+
+// Another function example
+function sing() {
+  console.log("DO");
+  console.log("RE");
+  console.log("MI");
+}
+
+// 2. Run
+sing();
+
+// It's important to call the function, JavaScript will not run
+// it until we tell him to do so.
+
+sing(); // run it again!
+
+// Remember to define your functions first, and then run them.

fd6ca70e7551fd8467977adc459d22a904890eb5
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-12-26 14:01:39 -0600

    100. Function expressions exercise 02 concluded

---
+++ leveling up functions/function_expressions_exercise_02.js
@@ -0,0 +1,14 @@
+// Function Expression Exercise
+
+// Define a function that returns the square of a number.
+// Define the function as a function expression,
+// stored in a variable called square.
+
+square(4); //16
+square(3); //9
+
+// Answer:
+
+let square = function (num) {
+  return num * num;
+};

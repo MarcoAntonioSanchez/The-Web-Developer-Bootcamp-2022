5094bec6ab26eacd28582ca339419a5b51a48974
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2023-04-09 20:16:58 -0600

    151. Intro to events completed

---
+++ introducing_DOM/DOM_events/intro_to_events/app.js
@@ -0,0 +1,19 @@
+// EVENTS
+// Responding to user inputs and actions!
+
+// A small taste
+// clicks
+// drags
+// drops
+// hovers
+// scrolls
+// form submission
+// key presses
+// focus/blur
+// mouse wheel
+// double click
+// copying
+// pasting
+// audio start
+// screen resize
+// printing

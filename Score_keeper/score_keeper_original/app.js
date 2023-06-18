bfd1e48c397b03d6d8efd246fcf197d3a64094a5
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2023-06-18 12:01:26 -0600

    170. Updating the documentation on the score keeper code allong project

--- Score_keeper/score_keeper_original/app.js
+++ Score_keeper/score_keeper_original/app.js
@@ -12,7 +12,7 @@ let p2Score = 0;
 let winningScore = 3;
 let isGameOver = false;
 
-// First we add an event listener on the nomber 1 player button, then we run a function on that click event
+// First we add an event listener on the number 1 player button, then we run a function on that click event
 p1Button.addEventListener("click", function () {
   // Now we check if the game over status is not true
   if (!isGameOver) {
@@ -20,10 +20,12 @@ p1Button.addEventListener("click", function () {
     p1Score += 1;
     // Next we check if the player 1 score is strictly equals to the winnign score defined above
     if (p1Score === winningScore) {
-      // If it is the game over status value gets updated to true
+      // If it is, the game over status value gets updated to true
       isGameOver = true;
+      // And then we add the respective classes to each player, with the looser and winner colors
       p1Display.classList.add("has-text-success");
       p2Display.classList.add("has-text-danger");
+      // We also disabled buttons for both players
       p1Button.disabled = true;
       p2Button.disabled = true;
     }
@@ -32,6 +34,7 @@ p1Button.addEventListener("click", function () {
   }
 });
 
+// Logic for the player number 2 is just the same as the player number 1
 p2Button.addEventListener("click", function () {
   if (!isGameOver) {
     p2Score += 1;
@@ -46,21 +49,31 @@ p2Button.addEventListener("click", function () {
   }
 });
 
+// The winning score select input gets a event listener to run a function on values changes
 winningScoreSelect.addEventListener("change", function () {
+  // Function here will update the winning score variable value to the one selected via the html input
   winningScore = parseInt(this.value);
+  // The game gets reseted with a function
   reset();
 });
 
+// An event listener in the reset button, when clicked
 resetButton.addEventListener("click", reset);
 
+// Reset function starts with no data passed
 function reset() {
+  // Game over status variable is set to false
   isGameOver = false;
+  // Both scores are set to 0
   p1Score = 0;
   p2Score = 0;
+  // The display content of the scores gets updated to 0 as well
   p1Display.textContent = 0;
   p2Display.textContent = 0;
+  // And the the winner and looser color classes are removed
   p1Display.classList.remove("has-text-success", "has-text-danger");
   p2Display.classList.remove("has-text-success", "has-text-danger");
+  // For least, both player buttons are enabled for a new game to start.
   p1Button.disabled = false;
   p2Button.disabled = false;
 }
--- Score_keeper/score_keeper_refactored/app.js
+++ Score_keeper/score_keeper_refactored/app.js
@@ -1,3 +1,7 @@
+// This is a refactored version of the score keeper code allong project
+// Resulting in a shorter syntax and optimized performance re using code
+
+// First we set the html tags into object properties, inside each player constants
 const p1 = {
   score: 0,
   button: document.querySelector("#p1Button"),
@@ -10,43 +14,60 @@ const p2 = {
   display: document.querySelector("#p2Display"),
 };
 
+// Then we add the html reset button and select input into constants
 const resetButton = document.querySelector("#reset");
 const winningScoreSelect = document.querySelector("#playTo");
 
+// The variables for both player's score are intialized in 0, winning score is set to 3 by default and the game over status variable is initialized as false
 let p1Score = 0;
 let p2Score = 0;
 let winningScore = 3;
 let isGameOver = false;
 
+// We start a function for update the scores of both players, with 2 arguments passed: Player and Opponent
 function updateScores(player, opponent) {
+  // First we check if the game over status is false to keep running the function's code
   if (!isGameOver) {
+    // Then we add the player a score of 1
     player.score += 1;
+    // We check if the player score is strictly equals t the winning score set with the select input
     if (player.score === winningScore) {
+      // If the score is equals, the game over status is updated to true
       isGameOver = true;
+      // And both the player and opponents get the respective winning and looser color classes
       player.display.classList.add("has-text-success");
       opponent.display.classList.add("has-text-danger");
+      // Both player buttons are disabled
       player.button.disabled = true;
       opponent.button.disabled = true;
     }
+    // And the display score tag gets updated
     player.display.textContent = player.score;
   }
 }
 
+// We add an event listener on click to start the update function with p1 as player and p2 as opponent
 p1.button.addEventListener("click", function () {
   updateScores(p1, p2);
 });
 
+// Then we add another event listener on click to the player 2 button, but with the p2 as player and the p1 as opponent
 p2.button.addEventListener("click", function () {
   updateScores(p2, p1);
 });
 
+// The winnins score selection event listener on change keeps the same syntax
 winningScoreSelect.addEventListener("change", function () {
   winningScore = parseInt(this.value);
+  // Resetting the game once the winning scode selection is updated
   reset();
 });
 
+// Same event listener in click for the above funciont in the reset button
 resetButton.addEventListener("click", reset);
 
+// For least, the reset function, wich major changes are the both player's beign managed as objects
+// So the attributes inside each player are controled ussing the p variable for the handle of both players objects
 function reset() {
   isGameOver = false;
   for (let p of [p1, p2]) {
